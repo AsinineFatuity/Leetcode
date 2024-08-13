8f84b7fc0b85da035f2c620b08c49880acedc1b8
Kembo <asininefatuity@gmail.com>
2024-08-13 23:26:22 +0300

    added comments

--- 003_remove_duplicates.py
+++ 003_remove_duplicates.py
@@ -11,16 +11,20 @@ class RecursiveSolution:
                     return self.removeDuplicates(nums)
         return len(nums)
 
+
 class NonRecursiveSolution:
     def removeDuplicates(self, nums: List[int]) -> int:
         if not nums:
             return 0
-        to_insert_idx = 1 #initialize the index to insert the next unique element
-        for i in range(1, len(nums)): # Loop through the list starting from the second element
+        to_insert_idx = 1  # initialize the index to insert the next unique element
+        for i in range(
+            1, len(nums)
+        ):  # Loop through the list starting from the second element
             if nums[i] != nums[i - 1]:
                 nums[to_insert_idx] = nums[i]
                 to_insert_idx += 1
         return to_insert_idx
+
 
 sln_classes = [RecursiveSolution, NonRecursiveSolution]
 for cls in sln_classes:
@@ -28,4 +32,4 @@ for cls in sln_classes:
     to_test_lists = [[1, 1, 2], [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]]
     for i in to_test_lists:
         k = sln.removeDuplicates(i)
-        print(f"k: {k}")
+        print(f"{cls.__name__}, k: {k}")
