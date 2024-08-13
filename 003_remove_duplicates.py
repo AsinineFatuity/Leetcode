81ac5dc0218e9f3eceb901691517772a45624fe9
Kembo <asininefatuity@gmail.com>
2024-08-13 22:56:51 +0300

    added non recursive solution

--- 003_remove_duplicates.py
+++ 003_remove_duplicates.py
@@ -1,7 +1,7 @@
 from typing import List
 
 
-class Solution:
+class RecursiveSolution:
     def removeDuplicates(self, nums: List[int]) -> int:
         for idx, cur_el in enumerate(nums):
             if idx != len(nums) - 1:
@@ -11,9 +11,21 @@ class Solution:
                     return self.removeDuplicates(nums)
         return len(nums)
 
+class NonRecursiveSolution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        to_insert_idx = 1 #initialize the index to insert the next unique element
+        for i in range(1, len(nums)): # Loop through the list starting from the second element
+            if nums[i] != nums[i - 1]:
+                nums[to_insert_idx] = nums[i]
+                to_insert_idx += 1
+        return to_insert_idx
 
-sln = Solution()
-to_test_lists = [[1, 1, 2], [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]]
-for i in to_test_lists:
-    k = sln.removeDuplicates(i)
-    print(f"k: {k}")
+sln_classes = [RecursiveSolution, NonRecursiveSolution]
+for cls in sln_classes:
+    sln = cls()
+    to_test_lists = [[1, 1, 2], [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]]
+    for i in to_test_lists:
+        k = sln.removeDuplicates(i)
+        print(f"k: {k}")
