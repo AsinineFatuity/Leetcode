d443e773363d855d8fe8335fce6a69241fe8ff37
Kembo <asininefatuity@gmail.com>
2024-08-19 22:37:16 +0300

    solved

--- 004_remove_duplicatesII.py
+++ 004_remove_duplicatesII.py
@@ -1,6 +1,7 @@
 """
 https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/?envType=study-plan-v2&envId=top-interview-150
 """
+
 from typing import List
 
 
---
+++ 005_majority_element.py
@@ -0,0 +1,16 @@
+"""
+https://leetcode.com/problems/majority-element/description/?envType=study-plan-v2&envId=top-interview-150
+"""
+
+from typing import List
+
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        count = 0
+        candidate = None
+        for num in nums:
+            if count == 0:
+                candidate = num
+            count += 1 if candidate == num else -1
+        return candidate
